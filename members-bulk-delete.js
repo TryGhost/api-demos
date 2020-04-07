124d50203a85d5714acfaf3395a07e5c7f33bd93
Hannah Wolfe <erisds@gmail.com>
2020-04-07 19:21:25 +0100

    made members bulk delete more robust

--- members-bulk-delete.js
+++ members-bulk-delete.js
@@ -3,7 +3,10 @@
  *
  * Usage:
  *
- * node members-bulk-delete.js https://blah.ghost.io ADMIN_API_KEY
+ * node members-bulk-delete.js https://blah.ghost.io ADMIN_API_KEY [doDelete]
+ *
+ * If you run this script with just a URL and key, it will do a dry run
+ * If you run this script with an extra argument (e.g. true) the deletions will be executed
  */
 
 if (process.argv.length < 4) {
@@ -13,6 +16,13 @@ if (process.argv.length < 4) {
 
 const url = process.argv[2];
 const key = process.argv[3];
+const doDelete = process.argv[4];
+
+if (!doDelete) {
+    console.log('Dry run...');
+} else {
+    console.log('Will do deletions!');
+}
 
 const Promise = require('bluebird');
 const GhostAdminAPI = require('@tryghost/admin-api');
@@ -25,18 +35,37 @@ const api = new GhostAdminAPI({
 (async function main() {
     try {
         const allMembers = await api.members.browse({limit: 'all'});
-        const deletePromises = [];
 
-        allMembers.forEach((member) => {
+        console.log(allMembers);
+
+        const freeMembers = allMembers.filter((member) => {
             // Comped members should have a subscription, but just in case
             if (!member.comped && member.stripe.subscriptions.length === 0) {
-                console.log('deleting', member.email);
-                deletePromises.push(api.members.delete({id: member.id}));
+                console.log('Will delete', member.email);
+                return true;
             }
+
+            console.log('Will keep', member.email);
+
+            return false;
         });
 
-        await Promise
-            .all(deletePromises);
+        console.log(freeMembers.length, 'Members will be deleted');
+
+        const result = await Promise.mapSeries(freeMembers, async (member) => {
+            console.log('Deleting', member.email);
+            let result = {};
+
+            // Call the API
+            if (doDelete) {
+                result = await api.members.delete({id: member.id});
+            }
+
+            // Add a delay but return the original result
+            return Promise.delay(50).return(result);
+        });
+
+        console.log('Deleted', result.length, 'members');
     } catch (err) {
         console.error('There was an error', require('util').inspect(err, false, null));
         process.exit(1);
